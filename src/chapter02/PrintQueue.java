b02ce139c664bc93f4eec1b2c418ae8fb4a7fcf1
cokepluscarbon <93948886@qq.com>
2014-03-11 19:21:56 +0800

    Lock

---
+++ src/chapter02/PrintQueue.java
@@ -0,0 +1,50 @@
+package chapter02;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintQueue {
+	private final Lock queueLock = new ReentrantLock();
+
+	public final static void main(String[] args) {
+		PrintQueue printQueue = new PrintQueue();
+
+		Thread[] threads = new Thread[10];
+		for (int i = 0; i < 10; i++) {
+			threads[i] = new Thread(new Job(printQueue));
+		}
+
+		for (int i = 0; i < 10; i++) {
+			threads[i].start();
+		}
+	}
+
+	public void printJob(Object document) {
+		queueLock.lock();
+		try {
+			Long duration = (long) (Math.random() * 10000);
+			System.out.println(Thread.currentThread().getName() + ": PrintQueue: Printing a Job during " + (duration / 1000) + " seconds");
+			Thread.sleep(duration);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			queueLock.unlock();
+		}
+	}
+}
+
+class Job implements Runnable {
+	private PrintQueue printQueue;
+
+	public Job(PrintQueue printQueue) {
+		this.printQueue = printQueue;
+	}
+
+	@Override
+	public void run() {
+		System.out.printf("%s: Going to print a document\n", Thread.currentThread().getName());
+		printQueue.printJob(new Object());
+		System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+	}
+
+}
