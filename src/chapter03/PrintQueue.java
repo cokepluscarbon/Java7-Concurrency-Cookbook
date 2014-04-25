7b6369dbcfbbfa8b67cbf61441235fc1c25263cb
cokepluscarbon <93948886@qq.com>
2014-04-25 10:58:44 +0800

    信号灯Semaphore

---
+++ src/chapter03/PrintQueue.java
@@ -0,0 +1,52 @@
+package chapter03;
+
+import java.util.concurrent.Semaphore;
+
+public class PrintQueue {
+	private final Semaphore semaphore;
+
+	public PrintQueue() {
+		this.semaphore = new Semaphore(1);
+	}
+
+	public void printJob(Object document) {
+		try {
+			semaphore.acquire();
+			long duration = (long) (Math.random() * 1000);
+			System.out.printf("%s: PrintQueue: Printing a Job during %d millisecond\n", Thread.currentThread().getName(),
+					duration);
+			Thread.sleep(duration);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaphore.release();
+		}
+	}
+
+	public static void main(String[] args) {
+		PrintQueue printQueue = new PrintQueue();
+		Thread thread[] = new Thread[10];
+		for (int i = 0; i < 10; i++) {
+			thread[i] = new Thread(new Job(printQueue), "Thread" + i);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			thread[i].start();
+		}
+	}
+}
+
+class Job implements Runnable {
+	private PrintQueue printQueue;
+
+	public Job(PrintQueue printQueue) {
+		this.printQueue = printQueue;
+	}
+
+	@Override
+	public void run() {
+		System.out.printf("%s: Going to print a job\n", Thread.currentThread().getName());
+		printQueue.printJob(new Object());
+		System.out.printf("%s: The document has been printed\n", Thread.currentThread().getName());
+	}
+}
