a80d256e709a1ef5c20fed8e75b9a6709dbb1306
cokepluscarbon <93948886@qq.com>
2014-05-04 16:20:38 +0800

    Multi Semaphore

--- src/chapter03/PrintQueue.java
+++ src/chapter03/PrintQueue.java
@@ -10,7 +10,7 @@ public class PrintQueue {
 	private Lock lockPrinters;
 
 	public PrintQueue() {
-		this.semaphore = new Semaphore(1);
+		this.semaphore = new Semaphore(3);
 		this.freePrinters = new boolean[3];
 		for (int i = 0; i < 3; i++) {
 			freePrinters[i] = true;
@@ -21,15 +21,35 @@ public class PrintQueue {
 	public void printJob(Object document) {
 		try {
 			semaphore.acquire();
+			int assignedPrinter = getPrinter();
 			long duration = (long) (Math.random() * 1000);
 			System.out.printf("%s: PrintQueue: Printing a Job during %d millisecond\n", Thread.currentThread()
 					.getName(), duration);
 			Thread.sleep(duration);
+			freePrinters[assignedPrinter] = true;
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
 			semaphore.release();
 		}
+	}
+
+	private int getPrinter() {
+		int ret = -1;
+		try {
+			lockPrinters.lock();
+			for (int i = 0; i < freePrinters.length; i++) {
+				if (freePrinters[i]) {
+					ret = i;
+					freePrinters[i] = false;
+					break;
+				}
+			}
+			lockPrinters.unlock();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ret;
 	}
 
 	public static void main(String[] args) {
